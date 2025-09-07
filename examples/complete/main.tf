499b1bdd00c04d1017fc74249e45930162e8e02c
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-09-07 08:19:18 +0100

    chore(deps): update terraform terraform-ibm-modules/secrets-manager/ibm to v2.8.10 (#9)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -48,7 +48,7 @@ locals {
 
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.8.9"
+  version              = "2.8.10"
   count                = var.existing_sm_guid == null ? 1 : 0
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
