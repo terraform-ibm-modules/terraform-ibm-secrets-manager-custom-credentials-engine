6af3286f3c7f3467e9fbb7bc340cea4ec225955a
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-10-25 09:18:30 +0100

    chore(deps): update terraform terraform-ibm-modules/code-engine/ibm to v4.6.8 (#25)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "code_engine" {
   source            = "terraform-ibm-modules/code-engine/ibm"
-  version           = "4.6.7"
+  version           = "4.6.8"
   resource_group_id = module.resource_group.resource_group_id
   project_name      = "${var.prefix}-project"
   jobs = {
