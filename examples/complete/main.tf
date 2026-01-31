f27994402bb01b480cf37de94b9c475b8b910a8d
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-31 22:30:05 +0000

    chore(deps): update dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "code_engine" {
   source            = "terraform-ibm-modules/code-engine/ibm"
-  version           = "4.7.25"
+  version           = "4.7.26"
   resource_group_id = module.resource_group.resource_group_id
   project_name      = "${var.prefix}-project"
   jobs = {
@@ -48,7 +48,7 @@ locals {
 
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.13.0"
+  version              = "2.13.1"
   count                = var.existing_sm_guid == null ? 1 : 0
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
