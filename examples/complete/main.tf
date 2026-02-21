6450d5f4d0ff68d42c0e02249fd791cc87c2a65b
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-02-21 19:16:52 +0000

    chore(deps): pin dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -48,7 +48,7 @@ locals {
 
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.13.4"
+  version              = "2.13.5"
   count                = var.existing_sm_guid == null ? 1 : 0
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
