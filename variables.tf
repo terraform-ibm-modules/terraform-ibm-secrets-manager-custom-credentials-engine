894fdefd932320f1473b2a238591e5667cc28483
mukul-palit <mukul.palit@ibm.com>
2026-08-19 01:29:16 +0530

    Correct variable description

--- variables.tf
+++ variables.tf
@@ -77,12 +77,12 @@ variable "iam_credential_secret_group_id" {
 
 variable "iam_credential_secret_ttl" {
   type        = string
-  description = "Specify validity / lease duration of ServiceID API key. Accepted values and formats are: SECONDS, Xm or Xh (where X is the number of minutes or hours appended to m or h respectively)."
+  description = "Specify validity / lease duration of ServiceID API key in seconds. Must be an integer string between 60 and 7776000 (90 days)."
   default     = "7776000" #tfsec:ignore:general-secrets-no-plaintext-exposure Default set to 90days
 
   validation {
     condition     = (var.iam_credential_secret_ttl >= 60) && (var.iam_credential_secret_ttl <= 7776000)
-    error_message = "TTL must be between 60 seconds and 7776000 seconds (90 days)."
+    error_message = "time-to-live (TTL) must be between 60 seconds and 7776000 seconds (90 days)."
   }
 }
 
